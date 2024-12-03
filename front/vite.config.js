44936edb3a047b78a7febae068c7e4d3237e894e
Gabriela82526 <sbohorquez52@uan.edu.co>
2024-12-02 19:23:29 -0500

    Vercel I

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,14 +4,19 @@ import vue from '@vitejs/plugin-vue';
 
 
 
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
 export default defineConfig({
   plugins: [vue()],
   base: '/mi-app/',
   resolve: {
     alias: {
-      build: '/src/build',
-      outdir: '/dist',
-      '@': '/src',
+     
+      build: path.resolve(__dirname, 'src/build'),
+      outdir: path.resolve(__dirname, 'dist'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
